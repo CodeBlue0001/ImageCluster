1704cbdf86359dbcc3b30a1b21abea865e36cbd2
CodeBlue0001 <145474893+CodeBlue0001@users.noreply.github.com>
2026-08-13 11:36:39 +0530

    adding the database handeling function commit 1

---
+++ backend/ai/save_face_data.py
@@ -0,0 +1,48 @@
+
+'''a function that store the face data and landmarks along with lable and store
+   the data as an numpy array in the database'''
+import pickle
+import sqlite3
+import numpy as np
+
+def init_db():
+    
+    db=sqlite3.connect("face_data.db")
+    cursor=db.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS face_data(face_landmarks BLOB,label TEXT,name TEXT)")
+    db.commit()
+    db.close()
+
+def find_face_data(face_landmarks):
+    init_db()
+    db=sqlite3.connect("face_data.db")
+    cursor=db.cursor()
+    cursor.execute("SELECT * FROM face_data")
+    face_data=cursor.fetchall()
+    db.close()
+    
+    for face_data in face_data:
+        face_landmarks=pickle.loads(face_data[0])
+        label=face_data[1]
+        name=face_data[2]
+        print(face_landmarks)
+        print(label)
+        print(name)
+    return face_data
+
+    
+def save_face_data(face_landmarks,label,name):
+    
+    init_db()
+    face_landmarks=np.array(face_landmarks)
+    # print(face_landmarks)
+    face_landmarks=pickle.dumps(face_landmarks)
+    db=sqlite3.connect("face_data.db")
+    cursor=db.cursor()
+    cursor.execute("INSERT INTO face_data(face_landmarks,label,name) VALUES (?,?,?)",(face_landmarks,label,name))
+    db.commit()
+    db.close()
+
+
+
+        
